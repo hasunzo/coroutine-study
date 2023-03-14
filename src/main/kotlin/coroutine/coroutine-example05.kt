685e3f467e11aa11eca9c4beaf9251e51da0ffe1
hasunzo <gktjsdl470@gmail.com>
2023-03-14 22:31:28 +0900

    Initial commit

---
+++ src/main/kotlin/coroutine/coroutine-example05.kt
@@ -0,0 +1,24 @@
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        println("Custom scope start")
+
+        coroutineScope {
+            launch {
+                delay(100)
+                println("Task 1 finished")
+            }
+
+            launch {
+                delay(100)
+                println("Task 2 finished")
+            }
+        }
+
+        println("Custom scope end")
+    }
+}
---
+++ src/main/kotlin/coroutine_life_cycle/coroutine-example06.kt
@@ -0,0 +1,17 @@
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        val job = launch(start = CoroutineStart.LAZY) {
+            println("Job started")
+        }
+
+        delay(100)
+
+        println("Preparing to start...")
+        job.start()
+    }
+}
